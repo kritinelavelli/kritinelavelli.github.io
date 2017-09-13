90d4cc263c096c714369dcd6b196992f916d7b9a
nkriti92 <nkriti92@gmail.com>
2017-09-13 01:34:19 -0400

    Added Images

--- src/nkriti92-github-io-app/nkriti92-github-io-app.js
+++ src/nkriti92-github-io-app/nkriti92-github-io-app.js
@@ -8,10 +8,19 @@
       }
       static get properties() {
         return {
-          // prop1: {
-          //   type: String,
-          //   value: 'Hello My App'
-          // }
+          image: {
+            type: String
+          },
+          routeData: {
+            type: Object,
+            observer: '_routeDataObserver'
+          },
+          title: {
+            type: String
+          },
+          subtitle: {
+            type: String
+          }
         };
       }
       openPreview() {
@@ -20,6 +29,33 @@
       }
       closePreview() {
         this.$.plain.close();
+      }
+      _routeDataObserver(newValue, oldValue) {
+        if (newValue.view == 'hvz') {
+          this.image = '/img/IMG_7873.JPG';
+          this.title = 'Humans vs Zombies';
+          this.subtitle = 'A User Centered Design Project';
+        }
+        else if (newValue.view == 'gameDesign' ) {
+          this.image = '';
+          this.title = 'HellWorld';
+          this.subtitle = 'Video Game Design';
+        }
+        else if (newValue.view == 'craigslist' ) {
+          this.image = '';
+          this.title = 'Craigslist Redesign';
+          this.subtitle = 'Craigslist for Students ';
+        }
+        else if (newValue.view == 'globalWarming' ) {
+          this.image = '';
+          this.title = 'Climate Change and Global Warming';
+          this.subtitle = 'Data Visualization';
+        }
+        else {
+          this.image = '';
+          this.title = 'Kriti Nelavelli';
+          this.subtitle = 'Design + Build';
+        }
       }
     }
 
